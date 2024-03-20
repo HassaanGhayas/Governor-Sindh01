9e0d23e1587a465772512dba7844d050bfaeef93
HassaanGhayas <160322281+HassaanGhayas@users.noreply.github.com>
2024-03-21 00:30:15 +0500

    45 Assignments for Practice

---
+++ 45 Assignments (Pending)/app.js
@@ -0,0 +1,71 @@
+// 45 ASSIGNMENTS (Pending)
+// DAY 1:
+// Q2
+// let a : string = "Asharib,";
+// console.log("Hello", a ,"would you like to learn some typescript");
+//Q3
+// let b: string = "asharib"
+// let uppercase = b
+// console.log(uppercase.toUpperCase())
+// let lowercase = b
+// console.log(lowercase.toLowerCase())
+// let titlecase = b
+// console.log(titlecase.replace("a", "A"));
+// DAY 2
+//Q4
+// console.log("Albert Einstein once said, 'A person who never made a mistake never tried anything new.'");
+//Q5
+// let mquote: string = "Albert Einstein once said,"
+// let msgs : string = mquote + " 'A person who never made a mistake never tried anything new.'"
+// console.log(msgs);
+//Q6
+// let wspace: string = "\t\n Hello \t\n"
+// console.log(wspace);
+// let woutspace: string = "Hello"
+// console.log(woutspace);
+//DAY 3
+//Q7
+// console.log(2*4);
+// console.log(16/2);
+// console.log(10-2);
+// console.log(6+2);
+//Q8
+// console.log(64/8);
+// console.log(2**3);
+// console.log(12-4);
+// console.log(10+2-4);
+//Q9
+// let magna: string = 2 + " is my favorite number"
+// console.log(magna);
+//DAY 4
+//Q10
+// Done
+//Q11
+// let nameoffriends : string[] = ["jack", "jill", "Phil"]
+// for (let i = 0; i < nameoffriends.length; i++) {
+//     console.log(nameoffriends[i]); 
+// }
+//Q12
+// let Friends: string[] = ["Jones", "Daniel", "James"]
+// console.log(Friends[0], "Goodbye");
+// console.log(Friends[1], "Goodbye");
+// console.log(Friends[2], "Goodbye");
+// DAY 5
+//Q13
+// let Vehicles: string[] = ["Kawasaki Motorcycle", "Honda Civic"]
+// Vehicles.forEach(Vehicles =>{
+// console.log("I would buy a", Vehicles);})
+// Q14
+// let invitation: string[] = ["Cousin1.", "Cousin2.", "Cousin3."]
+// invitation.forEach(invitation => {
+//     console.log("Hey", invitation.replace(".", ","),"you are invited at XYZ Lawn by ABC"); 
+// })
+// Q15
+// let invitation: string[] = ["Cousin1", "Cousin2", "Cousin3"]
+// let Cantattend: string[] = ["Cousin2"]
+// console.log(invitation[1], "can't attend");
+// invitation.splice(1, 1, "Cousin")
+// invitation.forEach(invitation => {
+//  console.log("Hey", invitation.replace(".", ","),"you are invited at XYZ Lawn by ABC"); 
+//     })
+export {};
