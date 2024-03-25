a909edc0251b33187abd2fc34a73332111a1ebf7
HassaanGhayas <160322281+HassaanGhayas@users.noreply.github.com>
2024-03-26 04:57:27 +0500

    updates

---
+++ Number Guessing Game/main.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+let randomNumber = Math.floor(Math.random() * 10 + 1);
+const answer = await inquirer.prompt([
+    { name: "usersGuess", type: "number", message: "Enter your guess:", }
+]);
+console.log(`You sure it was ${answer.usersGuess}?`);
+if (answer.usersGuess === randomNumber) {
+    console.log(`Yep u weren't wrong \n Congrats`);
+}
+else {
+    console.log(`Nah it was ${randomNumber} \n Better Luck Next Time!`);
+}
